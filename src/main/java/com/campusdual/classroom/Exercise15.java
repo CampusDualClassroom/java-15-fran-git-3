4e824d423633a7654a5b98e544179deb7dd8ca05
Fran <franrego333@gmail.com>
2024-09-26 14:17:14 +0200

    Ejercicio 15 completado

--- src/main/java/com/campusdual/classroom/Exercise15.java
+++ src/main/java/com/campusdual/classroom/Exercise15.java
@@ -7,8 +7,7 @@ public class Exercise15 {
 	public static void main(String[] args) {
 		FreshMerchandise fm = new FreshMerchandise("Manzanas", "001-9", "Frutería de ayer S.L.", 8, "C", "114D", 53,
 				Calendar.getInstance().getTime());
-		fm.printSpecificData();
-		System.out.println(fm.getLocation());
+				fm.printSpecificData();
 
 	}
 
--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,38 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    private Date expirationDate; // Propiedad para la fecha de caducidad
+
+    // Constructor
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate; // Inicializa la fecha de caducidad
+    }
+
+    // Método para obtener la fecha de caducidad
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    // Método para formatear la fecha
+    public String getFormattedDate(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(date);
+    }
+
+    // Método que devuelve los datos específicos como un String
+    public String getSpecificData() {
+        // Construir el String con la información específica
+        StringBuilder sb = new StringBuilder();
+        sb.append("Localización: ").append(getLocation()).append("\n");
+        sb.append("Fecha de caducidad: ").append(getFormattedDate(expirationDate));
+
+        return sb.toString();
+    }
+
+    // Método para imprimir los datos específicos
+    public void printSpecificData() {
+        // Invoca al método getSpecificData y muestra el resultado
+        System.out.println(getSpecificData());
+    }
 }
